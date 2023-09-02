fd646043d588b6d47c05c789de29c3e731bf7522
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-09-01 22:54:12 -0700

    add unplugin-icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import SvelteLogo from 'virtual:icons/logos/svelte'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+
+<SvelteLogo style="font-size:8em" />
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
