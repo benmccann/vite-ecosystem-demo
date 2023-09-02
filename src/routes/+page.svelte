f460acadca638e666a15d1d7d899efdf651f9af3
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-09-02 11:06:45 -0700

    add image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-    import SvelteLogo from 'virtual:icons/logos/svelte'
+    import SvelteLogo from 'virtual:icons/logos/svelte';
+    import Image from '$lib/Image.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+
+<Image src="./rich.jpeg" alt="Rich Harris" />
 
 <SvelteLogo style="font-size:8em" />
 
